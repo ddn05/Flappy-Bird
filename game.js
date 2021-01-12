6d793d9c790195c2a945cd3cd256f3f29e0b7c8a
ddn05 <dadannazril@gmail.com>
2021-01-12 11:15:18 +0700

    p

--- game.js
+++ game.js
@@ -1,1 +1,72 @@
-// JAVASCRIPT CODE //
+//pilih canvas
+const cvs = document.getElementById("bird")
+const ctx = cvs.getContext("2d")
+
+//membuat variabel
+let frames = 0;
+
+//load gambar sprite
+const sprite = new Image();
+sprite.src = "img/sprite.png";
+
+//background
+const bg = {
+    sX : 0,
+    sY : 0,
+    w : 275,
+    h : 226,
+    x : 0,
+    y : cvs.height - 226,
+    
+    draw : function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+}
+
+//foreground
+const fg = {
+    sX: 276,
+    sY: 0,
+    w: 224,
+    h: 112,
+    x: 0,
+    y: cvs.height - 112,
+        
+    draw : function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);     
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+
+
+}
+
+//bird
+const bird = {
+    animation : [
+        
+    ]
+}
+
+//draw
+function draw(){
+    ctx.fillStyle = "#70c5ce";
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
+    
+    bg.draw();
+    fg.draw();
+}
+
+//update
+function update(){
+
+}
+
+//loop
+function loop(){
+    update();
+    draw();
+    frames++;
+
+    requestAnimationFrame(loop);
+}
